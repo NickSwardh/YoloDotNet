1522a311d81716533bf8783e3a10ba598f053524
Nick Swardh <nickswardh@gmail.com>
2023-11-10 17:24:41 +0100

    Added video inference example

--- Console/Program.cs
+++ Console/Program.cs
@@ -2,43 +2,115 @@
 using SixLabors.ImageSharp.PixelFormats;
 using YoloDotNet;
 using YoloDotNet.Extensions;
+using YoloDotNet.Models;
 
 // Instantiate a new Yolo object
 using var yolo = new Yolo(@"path\to\model.onnx");
 
-// Load image
-using var image = Image.Load<Rgba32>(@"path\to\image.jpg");
+// Run inference on image
+RunInferenceOnImage(yolo, @"path\to\image.jpg");
 
-// Run inference
-var detections = yolo.RunInference(image);
+// Run inference on video
+RunInferenceOnVideo(yolo, new VideoOptions
+{
+    VideoFile = @"path\to\video.mp4",
+    OutputDir = @"path\to\outputfolder"
+});
 
-// Draw boxes
-image.DrawBoundingBoxes(detections);
+// Display ONNX metadata
+DisplayOnnxMetaData(yolo);
 
-// Save image
-image.Save(@"save\image.jpg");
+#region Methods
+static void RunInferenceOnImage(Yolo yolo, string imgPath)
+{
+    Console.WriteLine("Running inference on {0}\r\n", imgPath);
 
-// Optional - displaying ONNX metadata...
-#region Display internal ONNX parameters
+    // Load image
+    using var image = Image.Load<Rgba32>(imgPath);
+
+    // Run inference
+    var detections = yolo.RunInference(image);
+
+    // Draw boxes
+    image.DrawBoundingBoxes(detections);
+
+    var filename = Path.GetDirectoryName(imgPath) + @"\result.jpg";
+
+    // Save image
+    image.Save(filename);
+
+    Console.WriteLine("Done!");
+    Console.WriteLine("Saved as {0}", filename);
+}
+
+static void RunInferenceOnVideo(Yolo yolo, VideoOptions videoOptions)
+{
+    int currentLineCursor = 0;
+    Console.WriteLine();
+    Console.WriteLine(@"Running inference on {0}", videoOptions.VideoFile);
+    Console.CursorVisible = false;
+
+    // Listen to events...
+    yolo.VideoStatusEvent += OnStatusChangeEvent;
+    yolo.VideoProgressEvent += OnVideoProgressEvent;
+    yolo.VideoCompleteEvent += OnCompleteEvent;
+
+    // Run inference on video
+    try
+    {
+        yolo.RunInference(videoOptions);
+    }
+    catch (Exception ex)
+    {
+        Console.SetCursorPosition(20, currentLineCursor);
+        Console.Write("{0}", ex.Message);
+    }
+
+    static void OnCompleteEvent(object? sender, EventArgs e)
+    {
+        Console.WriteLine();
+        Console.WriteLine("All done!");
+    }
+
+    void OnStatusChangeEvent(object? sender, EventArgs e)
+    {
+        Console.WriteLine();
+        Console.Write((string)sender!);
+        currentLineCursor = Console.CursorTop;
+    }
+
+    void OnVideoProgressEvent(object? sender, EventArgs e)
+    {
+        Console.SetCursorPosition(20, currentLineCursor);
+        Console.Write(new string(' ', 4));
+        Console.SetCursorPosition(20, currentLineCursor);
+        Console.Write("{0}%", (int)sender!);
+    }
+
+    Console.WriteLine();
+    Console.CursorVisible = true;
+}
+
+static void DisplayOnnxMetaData(Yolo yolo)
 {
     Console.WriteLine();
     Console.WriteLine("Internal ONNX properties");
     Console.WriteLine(new string('-', 58));
 
-    // Display internal ONNX properties using reflection
+    // Display internal ONNX properties...
     foreach (var property in yolo.OnnxModel.GetType().GetProperties())
     {
         var value = property.GetValue(yolo.OnnxModel);
         Console.WriteLine($"{property.Name,-20}{value!}");
     }
 
-    // Print internal ONNX labels with added hexadecimal colors from YoloDotNet
     var labels = yolo.OnnxModel.Labels;
 
     Console.WriteLine();
     Console.WriteLine($"Labels ({labels.Length}):");
     Console.WriteLine(new string('-', 58));
 
+    // Display labels and its corresponding color
     for (var i = 0; i < labels.Length; i++)
     {
         // Capitalize first letter in label
