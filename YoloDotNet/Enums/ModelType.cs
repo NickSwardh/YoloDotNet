6b81c5f93d76b3a3866b3ecc4356629c59991a1d
Nick Swardh <nickswardh@gmail.com>
2024-01-17 12:23:42 +0100

    Edited intellisense comment

--- YoloDotNet/Enums/ModelType.cs
+++ YoloDotNet/Enums/ModelType.cs
@@ -1,5 +1,8 @@
 ﻿namespace YoloDotNet.Enums
 {
+    /// <summary>
+    /// Strongly typed names for image vision types.
+    /// </summary>
     public enum ModelType
     {
         Classification = 2,
--- YoloDotNet/Enums/VideoAction.cs
+++ YoloDotNet/Enums/VideoAction.cs
@@ -1,23 +1,15 @@
 ﻿namespace YoloDotNet.Enums
 {
+    /// <summary>
+    /// Strongly typed names for video pipeline stages.
+    /// </summary>
     public enum VideoAction
     {
-        // Get metadata from video
         GetMetaData,
-
-        // Extract  metadata
         ExtractMetaData,
-
-        // Extract audio file
         ExtractAudio,
-
-        // Extract frames
         ExtractFrames,
-
-        // Detecting objects
         ProcessFrames,
-
-        // Compiling frames to video
         CompileFrames
     }
 }
