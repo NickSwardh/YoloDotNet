9c7559a0811de1a52d557f1e496026ce3e7efd90
Nick Swardh <nickswardh@gmail.com>
2024-01-17 12:38:14 +0100

    Added intellisense comments

--- YoloDotNet/Models/Classification.cs
+++ YoloDotNet/Models/Classification.cs
@@ -1,9 +1,18 @@
-﻿
-namespace YoloDotNet.Models
+﻿namespace YoloDotNet.Models
 {
+    /// <summary>
+    /// Represents the result of image classification
+    /// </summary>
     public class Classification
     {
+        /// <summary>
+        /// Label of classified image.
+        /// </summary>
         public string Label { get; set; } = default!;
+
+        /// <summary>
+        /// Confidence score of classified image.
+        /// </summary>
         public double Confidence { get; set; }
     }
 }
