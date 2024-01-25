caa408ecba1940c74e1f4101cb74f6d804002419
Nick Swardh <nickswardh@gmail.com>
2024-01-25 15:07:18 +0100

    Added models for segmentation

---
+++ YoloDotNet/Models/Segmentation.cs
@@ -0,0 +1,27 @@
+﻿using SixLabors.ImageSharp;
+
+namespace YoloDotNet.Models
+{
+    public class Segmentation : IDetection
+    {
+        /// <summary>
+        /// Label information associated with the detected object.
+        /// </summary>
+        public LabelModel Label { get; init; } = new();
+
+        /// <summary>
+        /// Confidence score of the detected object.
+        /// </summary>
+        public double Confidence { get; init; }
+
+        /// <summary>
+        /// Rectangle defining the region of interest (bounding box) of the detected object.
+        /// </summary>
+        public Rectangle Rectangle { get; init; }
+
+        /// <summary>
+        /// Segmentated pixels (x,y) with the pixel confidence value
+        /// </summary>
+        public double[,] SegmentationMask { get; set; } = default!;
+    }
+}
