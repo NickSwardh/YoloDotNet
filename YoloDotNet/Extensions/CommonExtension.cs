1174dfef49c3009e11cf0382401beef7c94faed1
Nick Swardh <nickswardh@gmail.com>
2024-01-27 12:52:17 +0100

    Refactored method

--- YoloDotNet/Extensions/CommonExtension.cs
+++ YoloDotNet/Extensions/CommonExtension.cs
@@ -1,16 +1,18 @@
-﻿using System.Globalization;
+﻿using SixLabors.ImageSharp;
+using System.Globalization;
 
 namespace YoloDotNet.Extensions
 {
     public static class CommonExtension
     {
         /// <summary>
-        /// Calculate confidence score in percent
+        /// Converts a value to a string representation as a percentage with two decimal points.
         /// </summary>
-        /// <param name="confidence"></param>
-        /// <returns></returns>
-        public static string ToPercent(this double confidence)
-            => (confidence * 100).ToString("0.##", CultureInfo.InvariantCulture);
+        /// <param name="value">The value to convert.</param>
+        /// <returns>A string representation of the percentage with two decimal points.</returns>
+        public static string ToPercent(this double value)
+            => (value * 100).ToString("0.##", CultureInfo.InvariantCulture);
+
         /// <summary>
         /// Calculates new font size based on image DPI (dots per inch), image dimensions and the provided font size.
         /// If no DPI information is present in the image metadata, a default DPI of 72 is assumed.
