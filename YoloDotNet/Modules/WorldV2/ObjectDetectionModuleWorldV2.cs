7acc653e0d7bab35101bb45cc610396ac8f416e9
Nick Swardh <nickswardh@gmail.com>
2025-04-30 23:53:55 +0200

    Simplify video inference

--- YoloDotNet/Modules/WorldV2/ObjectDetectionModuleWorldV2.cs
+++ YoloDotNet/Modules/WorldV2/ObjectDetectionModuleWorldV2.cs
@@ -1,12 +1,8 @@
 ﻿namespace YoloDotNet.Modules.WorldV2
 {
-    public class ObjectDetectionModuleWorldV2 : IObjectDetectionModule
+    internal class ObjectDetectionModuleWorldV2 : IObjectDetectionModule
     {
         private readonly YoloCore _yoloCore;
-
-        public event EventHandler VideoProgressEvent = delegate { };
-        public event EventHandler VideoCompleteEvent = delegate { };
-        public event EventHandler VideoStatusEvent = delegate { };
 
         public OnnxModel OnnxModel => _yoloCore.OnnxModel;
         private readonly ObjectDetectionModuleV8 _objectDetectionModule = default!;
@@ -15,32 +11,16 @@
         {
             _yoloCore = yoloCore;
 
-            // Yolo World has the same model input/output shapes as Yolov8
+            // Yolo WorldV2 has the same model input/output shapes as Yolov8
             // Use Yolov8 module
             _objectDetectionModule = new ObjectDetectionModuleV8(_yoloCore);
-
-            SubscribeToVideoEvents();
         }
 
         public List<ObjectDetection> ProcessImage(SKImage image, double confidence, double pixelConfidence, double iou)
             => _objectDetectionModule.ProcessImage(image, confidence, pixelConfidence, iou);
 
-        public Dictionary<int, List<ObjectDetection>> ProcessVideo(VideoOptions options, double confidence, double pixelConfidence, double iou)
-            => _yoloCore.RunVideo(options, confidence, pixelConfidence, iou, ProcessImage);
-
-        private void SubscribeToVideoEvents()
-        {
-            _yoloCore.VideoProgressEvent += (sender, e) => VideoProgressEvent?.Invoke(sender, e);
-            _yoloCore.VideoCompleteEvent += (sender, e) => VideoCompleteEvent?.Invoke(sender, e);
-            _yoloCore.VideoStatusEvent += (sender, e) => VideoStatusEvent?.Invoke(sender, e);
-        }
-
         public void Dispose()
         {
-            _yoloCore.VideoProgressEvent -= (sender, e) => VideoProgressEvent?.Invoke(sender, e);
-            _yoloCore.VideoCompleteEvent -= (sender, e) => VideoCompleteEvent?.Invoke(sender, e);
-            _yoloCore.VideoStatusEvent -= (sender, e) => VideoStatusEvent?.Invoke(sender, e);
-
             _yoloCore?.Dispose();
 
             GC.SuppressFinalize(this);
