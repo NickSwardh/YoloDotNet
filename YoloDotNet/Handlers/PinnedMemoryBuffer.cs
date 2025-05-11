46292c603a00a4352a67b6406467d260cdf9530d
Nick Swardh <nickswardh@gmail.com>
2025-05-11 13:08:18 +0200

    Added a pinned memory buffer handler to improve performance in image resizing operations

---
+++ YoloDotNet/Handlers/PinnedMemoryBuffer.cs
@@ -0,0 +1,42 @@
+﻿namespace YoloDotNet.Handlers
+{
+    public class PinnedMemoryBuffer
+    {
+        public readonly SKImageInfo ImageInfo;
+        public readonly byte[] Buffer;
+        public readonly IntPtr Pointer;
+        public readonly SKBitmap TargetBitmap;
+        public readonly SKCanvas Canvas;
+
+        private readonly GCHandle _handle;
+
+        public PinnedMemoryBuffer(SKImageInfo imageInfo)
+        {
+            ImageInfo = imageInfo;
+
+            //var _imageInfo = new SKImageInfo(width, height, SKColorType.Rgb888x, SKAlphaType.Opaque);
+            Buffer = new byte[imageInfo.BytesSize];
+
+            _handle = GCHandle.Alloc(Buffer, GCHandleType.Pinned);
+            Pointer = _handle.AddrOfPinnedObject();
+
+            // Wrap the pinned buffer in a SKBitmap so we can draw into it
+            TargetBitmap = new SKBitmap();
+            if (!TargetBitmap.InstallPixels(imageInfo, Pointer, imageInfo.RowBytes))
+                throw new Exception("Failed to install pixels into SKBitmap");
+
+            Canvas = new SKCanvas(TargetBitmap);
+        }
+
+        public void Dispose()
+        {
+            Canvas?.Dispose();
+            TargetBitmap?.Dispose();
+
+            if (_handle.IsAllocated)
+                _handle.Free();
+
+            GC.SuppressFinalize(this);
+        }
+    }
+}
---
+++ YoloDotNet/Handlers/PinnedMemoryBufferPool.cs
@@ -0,0 +1,36 @@
+﻿namespace YoloDotNet.Handlers
+{
+    public class PinnedMemoryBufferPool
+    {
+        internal readonly ConcurrentBag<PinnedMemoryBuffer> _pool = [];
+        private readonly SKImageInfo _imageInfo;
+
+        public PinnedMemoryBufferPool(SKImageInfo skInfo, int initialSize = 20)
+        {
+            _imageInfo = skInfo;
+
+            for (int i = 0; i < initialSize; i++)
+                _pool.Add(new PinnedMemoryBuffer(_imageInfo));
+        }
+
+        public PinnedMemoryBuffer Rent()
+        {
+            if (_pool.TryTake(out var buffer))
+                return buffer;
+
+            return new PinnedMemoryBuffer(_imageInfo); // fallback
+        }
+
+        public void Return(PinnedMemoryBuffer buffer)
+        {
+            buffer.TargetBitmap.Erase(SKColors.Empty);
+            _pool.Add(buffer);
+        }
+
+        public void Dispose()
+        {
+            while (_pool.TryTake(out var buffer))
+                buffer.Dispose();
+        }
+    }
+}
