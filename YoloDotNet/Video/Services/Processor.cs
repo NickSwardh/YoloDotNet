41251cc1349f70bf238d6365c8d735dee82a50ab
Nick Swardh <nickswardh@gmail.com>
2025-07-12 00:31:55 +0200

    Add arguments to list safely

--- YoloDotNet/Video/Services/Processor.cs
+++ YoloDotNet/Video/Services/Processor.cs
@@ -2,21 +2,29 @@
 {
     public class Processor
     {
-        public static Process Create(string process, string[] arguments)
-            => new()
-            {
-                StartInfo = new ProcessStartInfo
-                {
-                    FileName = process,
-                    Arguments = string.Join(' ', arguments),
-                    UseShellExecute = false,
-                    RedirectStandardOutput = true,
-                    RedirectStandardError = true,
-                    RedirectStandardInput = true,
-                    CreateNoWindow = true
-                },
+        public static Process Create(string fileName, IEnumerable<string> arguments)
+        { 
+            var process = new Process()
+               {
+                   StartInfo = new ProcessStartInfo
+                   {
+                       FileName = fileName,
+                       UseShellExecute = false,
+                       RedirectStandardOutput = true,
+                       RedirectStandardError = true,
+                       RedirectStandardInput = true,
+                       CreateNoWindow = true
+                   },
 
-                EnableRaisingEvents = true,
-            };
+                   EnableRaisingEvents = true,
+               };
+
+            // Add each argument safely to ArgumentList
+            foreach (var arg in arguments)
+                process.StartInfo.ArgumentList.Add(arg);
+
+            return process;
+
+        }
     }
 }
