4039921959b5d418491eb4e19b0602a2d6d6c183
Nick Swardh <nickswardh@gmail.com>
2025-07-12 18:42:25 +0200

    Add custom confidence comparer

---
+++ YoloDotNet/Handlers/ConfidenceComparer.cs
@@ -0,0 +1,18 @@
+﻿namespace YoloDotNet.Handlers
+{
+    internal class ConfidenceComparer : IComparer<ObjectResult>
+    {
+        public static readonly ConfidenceComparer Instance = new();
+
+        public int Compare(ObjectResult? x, ObjectResult? y)
+        {
+            if (x is null) return 1;  // Nulls go last in descending order
+            if (y is null) return -1;
+
+            if (x.Confidence > y.Confidence) return -1;
+            if (x.Confidence < y.Confidence) return 1;
+
+            return 0;
+        }
+    }
+}
