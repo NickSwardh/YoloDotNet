25c44904184e5cc0d4f0ea7ffc96cc7d5ebeb9e8
Nick Swardh <nickswardh@gmail.com>
2025-07-17 12:14:01 +0200

    Extend enum with TensorRT FP32, FP16 and INT8 configurations

--- test/YoloDotNet.Benchmarks/Setup/YoloTypes.cs
+++ test/YoloDotNet.Benchmarks/Setup/YoloTypes.cs
@@ -8,68 +8,192 @@ namespace YoloDotNet.Benchmarks.Setup
     {
         V5u_Cls_CPU,
         V5u_Cls_GPU,
+        V5u_Cls_TRT32,
+        V5u_Cls_TRT16,
+        V5u_Cls_TRT8,
+
         V5u_Obj_CPU,
         V5u_Obj_GPU,
+        V5u_Obj_TRT32,
+        V5u_Obj_TRT16,
+        V5u_Obj_TRT8,
+
         V5u_Obb_CPU,
         V5u_Obb_GPU,
+        V5u_Obb_TRT32,
+        V5u_Obb_TRT16,
+        V5u_Obb_TRT8,
+
         V5u_Pos_CPU,
         V5u_Pos_GPU,
+        V5u_Pos_TRT32,
+        V5u_Pos_TRT16,
+        V5u_Pos_TRT8,
+
         V5u_Seg_CPU,
         V5u_Seg_GPU,
+        V5u_Seg_TRT32,
+        V5u_Seg_TRT16,
+        V5u_Seg_TRT8,
+
+        //
 
         V8_Cls_CPU,
         V8_Cls_GPU,
+        V8_Cls_TRT32,
+        V8_Cls_TRT16,
+        V8_Cls_TRT8,
+
         V8_Obj_CPU,
         V8_Obj_GPU,
+        V8_Obj_TRT32,
+        V8_Obj_TRT16,
+        V8_Obj_TRT8,
+
         V8_Obb_CPU,
         V8_Obb_GPU,
+        V8_Obb_TRT32,
+        V8_Obb_TRT16,
+        V8_Obb_TRT8,
+
         V8_Pos_CPU,
         V8_Pos_GPU,
+        V8_Pos_TRT32,
+        V8_Pos_TRT16,
+        V8_Pos_TRT8,
+
         V8_Seg_CPU,
         V8_Seg_GPU,
+        V8_Seg_TRT32,
+        V8_Seg_TRT16,
+        V8_Seg_TRT8,
+
+        //
 
         V9_Cls_CPU,
         V9_Cls_GPU,
+        V9_Cls_TRT32,
+        V9_Cls_TRT16,
+        V9_Cls_TRT8,
+
         V9_Obj_CPU,
         V9_Obj_GPU,
+        V9_Obj_TRT32,
+        V9_Obj_TRT16,
+        V9_Obj_TRT8,
+
         V9_Obb_CPU,
         V9_Obb_GPU,
+        V9_Obb_TRT32,
+        V9_Obb_TRT16,
+        V9_Obb_TRT8,
+
         V9_Pos_CPU,
         V9_Pos_GPU,
+        V9_Pos_TRT32,
+        V9_Pos_TRT16,
+        V9_Pos_TRT8,
+
         V9_Seg_CPU,
         V9_Seg_GPU,
+        V9_Seg_TRT32,
+        V9_Seg_TRT16,
+        V9_Seg_TRT8,
+
+        //
 
         V10_Cls_CPU,
         V10_Cls_GPU,
+        V10_Cls_TRT32,
+        V10_Cls_TRT16,
+        V10_Cls_TRT8,
+
         V10_Obj_CPU,
         V10_Obj_GPU,
+        V10_Obj_TRT32,
+        V10_Obj_TRT16,
+        V10_Obj_TRT8,
+
         V10_Obb_CPU,
         V10_Obb_GPU,
+        V10_Obb_TRT32,
+        V10_Obb_TRT16,
+        V10_Obb_TRT8,
+
         V10_Pos_CPU,
         V10_Pos_GPU,
+        V10_Pos_TRT32,
+        V10_Pos_TRT16,
+        V10_Pos_TRT8,
+
         V10_Seg_CPU,
         V10_Seg_GPU,
+        V10_Seg_TRT32,
+        V10_Seg_TRT16,
+        V10_Seg_TRT8,
+
+        //
 
         V11_Cls_CPU,
         V11_Cls_GPU,
+        V11_Cls_TRT32,
+        V11_Cls_TRT16,
+        V11_Cls_TRT8,
+
         V11_Obj_CPU,
         V11_Obj_GPU,
+        V11_Obj_TRT32,
+        V11_Obj_TRT16,
+        V11_Obj_TRT8,
+
         V11_Obb_CPU,
         V11_Obb_GPU,
+        V11_Obb_TRT32,
+        V11_Obb_TRT16,
+        V11_Obb_TRT8,
+
         V11_Pos_CPU,
         V11_Pos_GPU,
+        V11_Pos_TRT32,
+        V11_Pos_TRT16,
+        V11_Pos_TRT8,
+
         V11_Seg_CPU,
         V11_Seg_GPU,
+        V11_Seg_TRT32,
+        V11_Seg_TRT16,
+        V11_Seg_TRT8,
+
+        //
 
         V12_Cls_CPU,
         V12_Cls_GPU,
+        V12_Cls_TRT32,
+        V12_Cls_TRT16,
+        V12_Cls_TRT8,
+
         V12_Obj_CPU,
         V12_Obj_GPU,
+        V12_Obj_TRT32,
+        V12_Obj_TRT16,
+        V12_Obj_TRT8,
+
         V12_Obb_CPU,
         V12_Obb_GPU,
+        V12_Obb_TRT32,
+        V12_Obb_TRT16,
+        V12_Obb_TRT8,
+
         V12_Pos_CPU,
         V12_Pos_GPU,
+        V12_Pos_TRT32,
+        V12_Pos_TRT16,
+        V12_Pos_TRT8,
+
         V12_Seg_CPU,
         V12_Seg_GPU,
+        V12_Seg_TRT32,
+        V12_Seg_TRT16,
+        V12_Seg_TRT8,
     }
 }
