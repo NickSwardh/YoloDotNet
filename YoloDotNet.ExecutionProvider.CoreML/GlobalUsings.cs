dbe8660ddf921b82d890031214d3e70b4f079c8e
Nick Swardh <nickswardh@gmail.com>
2025-10-05 01:03:01 +0200

    Add CoreMLExecutionProvider for ONNX inference on macOS/iOS

---
+++ YoloDotNet.ExecutionProvider.CoreML/GlobalUsings.cs
@@ -0,0 +1,9 @@
+﻿global using System.Data;
+
+global using YoloDotNet.Core;
+global using YoloDotNet.Enums;
+global using YoloDotNet.Models;
+global using YoloDotNet.Models.Interfaces;
+
+global using Microsoft.ML.OnnxRuntime;
+global using Microsoft.ML.OnnxRuntime.Tensors;
