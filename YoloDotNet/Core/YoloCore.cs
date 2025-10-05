542e66c1d21020604fa5ab6e0401a1f3176b35dd
Nick Swardh <nickswardh@gmail.com>
2025-10-05 12:34:06 +0200

    Refactor: Simplify memory management by replace custom arry pools with ArrayPool<T>.Shared

--- YoloDotNet/Core/YoloCore.cs
+++ YoloDotNet/Core/YoloCore.cs
@@ -11,14 +11,8 @@ namespace YoloDotNet.Core
     {
         #region Fields
         private bool _isDisposed;
-
-        public ArrayPool<float> customSizeFloatPool = default!;
-        public ArrayPool<ushort> customSizeHalfPool = default!;
-        public ArrayPool<ObjectResult> customSizeObjectResultPool = default!;
         private PinnedMemoryBufferPool _pinnedMemoryPool = default!;
-
         private readonly object _progressLock = new();
-
         private SKImageInfo _imageInfo;
         private long[] _inputShape = default!;
         private int _inputShapeSize;
@@ -43,13 +37,6 @@ namespace YoloDotNet.Core
             _inputShape = OnnxModel.InputShape;
             _inputShapeSize = OnnxModel.InputShapeSize;
 
-            // Create custom array pools based on model input size and output channels
-            customSizeFloatPool = ArrayPool<float>.Create(CalculateBufferPoolSize(_inputShapeSize), 20); 
-            customSizeHalfPool = ArrayPool<ushort>.Create(CalculateBufferPoolSize(_inputShapeSize), 20);
-
-            // ObjectResult pool based on output channels
-            customSizeObjectResultPool = ArrayPool<ObjectResult>.Create(CalculateBufferPoolSize(OnnxModel.Outputs[0].Channels), maxArraysPerBucket: 10);
-
             var format = (OnnxModel.Input.Channels == 1) ? SKColorType.Gray8 : SKColorType.Rgb888x;
             _imageInfo = new SKImageInfo(OnnxModel.Input.Width, OnnxModel.Input.Height, format, SKAlphaType.Opaque);
 
@@ -67,12 +54,13 @@ namespace YoloDotNet.Core
         {
             lock (_progressLock)
             {
-            var pinnedBuffer = _pinnedMemoryPool.Rent();
-                var normalizedPixelsFloatBuffer = customSizeFloatPool.Rent(_inputShapeSize);
-                var normalizedPixelsUshortBuffer = customSizeHalfPool.Rent(_inputShapeSize);
+                var pinnedBuffer = _pinnedMemoryPool.Rent();
 
-            try
-            {
+                var normalizedPixelsFloatBuffer = ArrayPool<float>.Shared.Rent(_inputShapeSize);
+                var normalizedPixelsUshortBuffer = ArrayPool<ushort>.Shared.Rent(_inputShapeSize);
+
+                try
+                {
                     // Resize image to model input size and store in pinned buffer for faster access
                     var originalImageSize = YoloOptions.ImageResize == ImageResize.Proportional
                         ? image.ResizeImageProportional(YoloOptions.SamplingOptions, pinnedBuffer)
@@ -98,15 +86,16 @@ namespace YoloDotNet.Core
 
                     return inferenceResult;
                 }
-            finally
-            {
+                finally
+                {
                     // Return rented buffers to their respective pools wuithout clearing for performance.
                     // WARNING: Only disable clearing if data is overwritten on next use to avoid data leakage!
-                    customSizeFloatPool.Return(normalizedPixelsFloatBuffer, false);
-                    customSizeHalfPool.Return(normalizedPixelsUshortBuffer, false);
-                _pinnedMemoryPool.Return(pinnedBuffer);
+                    ArrayPool<float>.Shared.Return(normalizedPixelsFloatBuffer, false);
+                    ArrayPool<ushort>.Shared.Return(normalizedPixelsUshortBuffer, false);
+
+                    _pinnedMemoryPool.Return(pinnedBuffer);
+                }
             }
-        }
         }
 
         #region Helper methods
@@ -128,7 +117,7 @@ namespace YoloDotNet.Core
             // Sort by confidence
             MemoryExtensions.Sort(predictionSpan, ConfidenceComparer.Instance);
 
-            var buffer = customSizeObjectResultPool.Rent(totalPredictions);
+            var buffer = ArrayPool<ObjectResult>.Shared.Rent(totalPredictions);
 
             try
             {
@@ -154,7 +143,7 @@ namespace YoloDotNet.Core
             }
             finally
             {
-                customSizeObjectResultPool.Return(buffer, false);
+                ArrayPool<ObjectResult>.Shared.Return(buffer, false);
             }
         }
 
